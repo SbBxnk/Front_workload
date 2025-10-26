d99964f7d55f06e6af9eec6a808f33ecdf982b23
chayakorn phukhiao <chayakon@spacetrax.co>
2025-10-27 01:41:51 +0700

    fix select dropdown main task

--- src/app/admin/sub-task/subTaskComponents/SelectMainTask.tsx
+++ src/app/admin/sub-task/subTaskComponents/SelectMainTask.tsx
@@ -38,18 +38,20 @@ function SelectMainTask({
           onClick={() =>
             setOpenDropdown(openDropdown === 'mainTask' ? null : 'mainTask')
           }
-          className="flex w-full items-center justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-light text-gray-600 transition-colors duration-300 ease-in-out focus:border-blue-500 focus:outline-none dark:border-zinc-600 dark:bg-zinc-800 dark:text-gray-400"
+          className="flex w-full items-center justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-light text-gray-600 transition-colors duration-300 ease-in-out focus:border-blue-500 focus:outline-none dark:border-zinc-600 dark:bg-zinc-800 dark:text-gray-400 min-h-[40px]"
         >
-          {selectMainTask === null
-            ? 'เลือกภาระงานหลัก'
-            : mainTasks?.find((task) => task.task_name === selectMainTask)
-                ?.task_name || 'เลือกภาระงานหลัก'}
+          <span className="truncate flex-1 mr-2">
+            {selectMainTask === null
+              ? 'เลือกภาระงานหลัก'
+              : mainTasks?.find((task) => task.task_name === selectMainTask)
+                  ?.task_name || 'เลือกภาระงานหลัก'}
+          </span>
           <ChevronDown
             className={`h-4 w-4 text-gray-600 transition-transform duration-200 dark:text-zinc-600 ${openDropdown === 'mainTask' ? 'rotate-180' : ''}`}
           />
         </button>
         {openDropdown === 'mainTask' && mainTasks && (
-          <div className="absolute z-10 mt-2 max-h-36 w-full overflow-y-auto rounded-md border border-gray-300 bg-white shadow-lg dark:border-zinc-600 dark:bg-zinc-900">
+          <div className="absolute z-10 mt-2 max-h-36 w-full overflow-y-auto rounded-md border border-gray-300 bg-white shadow-lg dark:border-zinc-600 dark:bg-zinc-900 min-w-[300px]">
             {mainTasks.map((task) => (
               <div
                 key={task.task_id}
@@ -58,7 +60,9 @@ function SelectMainTask({
                   handleSelectMainTask(task.task_id, task.task_name)
                 }
               >
-                {task.task_name}
+                <span className="truncate block w-full">
+                  {task.task_name}
+                </span>
               </div>
             ))}
           </div>
